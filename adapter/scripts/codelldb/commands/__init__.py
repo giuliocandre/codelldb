c146845d0c96973548cacd533b824dd8063bd682
GC <gc@dev>
2025-06-25 13:17:25 +0000

    add watch_page and get_checkpoints command

--- adapter/scripts/codelldb/commands/__init__.py
+++ adapter/scripts/codelldb/commands/__init__.py
@@ -5,3 +5,5 @@ def register(debugger):
     debugger.HandleCommand('script import codelldb')
     debugger.HandleCommand('command script add -c codelldb.commands.DebugInfoCommand debug_info')
     debugger.HandleCommand('command script add -c codelldb.commands.NoFailCommand nofail')
+    debugger.HandleCommand('command script add -f debugger.api.watch_page watch_page')
+    debugger.HandleCommand('command script add -f debugger.api.get_checkpoints get_checkpoints')
